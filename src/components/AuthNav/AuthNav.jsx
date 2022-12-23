db99c92356b43a925ef48f0343b079c2e5c87b23
MakrFrost <frostmark@ukr.net>
2022-12-23 13:44:00 +0200

    upd

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,12 +1,17 @@
 import { NavLink } from 'react-router-dom';
+import { MdOutlineAppRegistration, MdLogin } from 'react-icons/md';
 
 export const AuthNav = () => {
   return (
     <>
       <NavLink className="register" to="/register">
         Register
+        <MdOutlineAppRegistration className="navigations-icons" />
       </NavLink>
-      <NavLink to="/login">Log In</NavLink>
+      <NavLink to="/login">
+        Log In
+        <MdLogin className="navigations-icons" />
+      </NavLink>
     </>
   );
 };
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,16 @@
 import { useAuth } from 'redux/auth/useAuth';
 import { NavLink } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
 
 export const Navigation = () => {
   const { isLoggedIn } = useAuth();
 
   return (
     <>
-      <NavLink to="/">Home</NavLink>
+      <NavLink to="/">
+        Home
+        <FaHome className="navigations-icons" />
+      </NavLink>
       {isLoggedIn && <NavLink to="/contacts">Contacts</NavLink>}
     </>
   );
