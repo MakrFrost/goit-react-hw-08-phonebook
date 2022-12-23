b4a196ba80797d3f839043d8b20169f10e84f16a
MakrFrost <frostmark@ukr.net>
2022-12-23 23:58:36 +0200

    add nicely,  curwa!

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { register } from 'redux/auth/operations';
 import css from './RegisterForm.module.css';
+import { AiFillEye, AiFillEyeInvisible, AiOutlineUser } from 'react-icons/ai';
 
 export const RegisterForm = () => {
   const dispatch = useDispatch();
+  const [typeInput, setTypeInput] = useState('password');
+  const [noPassId, setNoPassId] = useState('pass');
+  const [closedEyeIcon, setClosedEyeIcon] = useState(true);
 
-  function formSubmit(e) {
+  function LookPassword(e) {
+    if (e.currentTarget.id === 'pass') {
+      setTypeInput('text');
+      setNoPassId('noPass');
+      setClosedEyeIcon(false);
+    } else {
+      setTypeInput('password');
+      setNoPassId('pass');
+      setClosedEyeIcon(true);
+    }
+  }
+
+  function FormSubmit(e) {
     e.preventDefault();
     const form = e.currentTarget;
+    console.log(form);
 
     dispatch(
       register({
@@ -21,7 +39,7 @@ export const RegisterForm = () => {
   }
 
   return (
-    <form onSubmit={formSubmit} className={css.reg_form} autoComplete="on">
+    <form onSubmit={FormSubmit} className={css.reg_form}>
       <label className={css.reg_name}>
         Username
         <input
@@ -31,6 +49,7 @@ export const RegisterForm = () => {
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
+        <AiOutlineUser className={css.reg_icon_name} />
       </label>
       <label className={css.reg_mail}>
         Email
@@ -38,7 +57,20 @@ export const RegisterForm = () => {
       </label>
       <label className={css.reg_pass}>
         Password
-        <input type="password" name="password" required />
+        <input type={typeInput} name="password" required />
+        {closedEyeIcon ? (
+          <AiFillEyeInvisible
+            className={css.reg_icon_pass}
+            onClick={LookPassword}
+            id={noPassId}
+          />
+        ) : (
+          <AiFillEye
+            className={css.reg_icon}
+            onClick={LookPassword}
+            id={noPassId}
+          />
+        )}
       </label>
       <button type="submit">Registered</button>
     </form>
